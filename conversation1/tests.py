cd3dd0bc074623b2880777af13301e94dd61a116
mbanje <mbanjejvernack@gmail.com>
2013-07-13 16:42:17 +0200

    test (2) when JO is send as an answer

--- conversation1/tests.py
+++ conversation1/tests.py
@@ -6,12 +6,20 @@ Replace this with more appropriate tests for your application.
 """
 
 from rapidsms.tests.harness import RapidTest
+from conversation1.models import Questions, History
 
 class SendSMSTest(RapidTest):
 
 
-    def test_Accueil(self):
-        """Outbox should contain message explaining WELCOME TO RAPIDSMS"""
+    def test_when_JO_is_send_the_first_time(self):
+        """Outbox should contain message 'WELCOME TO RAPIDSMS' """
         self.receive('JO', self.lookup_connections('1')[0])
         self.assertEqual(self.outbound[0].text, 'WELCOME TO RAPIDSMS')
-		
+	def test_when_JO_is_send_as_an_answer(self):
+		"""Outbox should contain the next question """
+		h=History(question="q1",tel_num=tel,status=1)
+		h.save()
+		h1=History(question="q2",tel_num=tel,status=0)
+		h1.save()			
+		self.receive('JO', self.lookup_connections('1')[0])
+		self.assertEqual(self.outbound[0].text, h1.question)
